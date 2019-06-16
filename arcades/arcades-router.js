ec2535d4db467ca85ca846a4b26915a5ebfa7e54
Doug <me@douglasbowen.com>
2019-06-16 15:53:21 -0400

    add ability to update arcades

--- arcades/arcades-router.js
+++ arcades/arcades-router.js
@@ -13,6 +13,20 @@ router.get("/", (req, res) => {
 
 // restricted put (arcade)
 
+router.put("/:id", async (req, res) => {
+  try {
+    const api = await Arcades.update(req.params.id, req.body);
+
+    if (!req.body.arcadename) {
+      res.status(400).json({ message: "please add a name" });
+    } else {
+      res.status(200).json(api);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // restricted delete (arcade)
 
 router.delete("/:id", async (req, res) => {

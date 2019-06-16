49aee2eb2268b252843dffd47a8a213ae00f8c3b
Doug <me@douglasbowen.com>
2019-06-15 22:43:12 -0400

    add seed for users

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,13 +1,6 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("users").insert([
+    { username: "admin", password: "abc123", admin: 1 },
+    { username: "sammyboy", password: "abc123", admin: 0 }
+  ]);
 };

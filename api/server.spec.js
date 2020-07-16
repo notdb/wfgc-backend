183c546b0668eea3688b2c3660c160e4c5f1c446
Doug <me@douglasbowen.com>
2020-07-15 20:22:05 -0400

    fix server.spec.js

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const server = require('./server.js');
-const { resource } = require('../../enterprise_device/backend/api/server.js');
+
 
 describe('server.js', ()=>{
     describe('index route', ()=> {
